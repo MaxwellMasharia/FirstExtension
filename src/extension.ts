d119bd1bb1fa3467b566b64ecac0fa58b0f6758d
Maxwell Masharia <viaxender@gmail.com>
2020-07-10 15:12:31 +0300

    Created a separate webview.html file containing all the html,css,js code for the webview (#1)

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // vscode module contains the API for creating the extensions
 import * as vscode from "vscode";
 import * as fs from "fs";
-const path = require("path");
+import * as path from "path";
 
 // The activate function is called when the extension is activated
 export function activate(context: vscode.ExtensionContext) {
@@ -21,161 +21,48 @@ export function activate(context: vscode.ExtensionContext) {
    * ]
    */
 
-
   console.log("Extension Activated");
   // Get the path to the extension
-  const extensionPath = context.extensionPath;
-  console.log("Path", extensionPath);
-  let disposable = vscode.commands.registerCommand(
-    "firstextension.start",
-    () => {
-      vscode.window.showInformationMessage("The extension is started");
-    }
-  );
 
-  vscode.commands.registerCommand("firstextension.key", () => {
-    vscode.window.showInformationMessage("Keybinding is working");
-  });
+  let ds = vscode.commands.registerCommand("firstextension.start", () => {
+    vscode.window.showInformationMessage("Webview extension started");
 
-  vscode.commands.registerCommand("catcoding.start", () => {
     const pannel = vscode.window.createWebviewPanel(
-      "catcoding",
-      "Cat Coding",
+      "webview",
+      "Webview Extension",
       vscode.ViewColumn.One,
       {
         enableScripts: true,
       }
     );
 
-    setTimeout(() => {
-      pannel.webview.postMessage({
-        message: "Maxwells Loves Java and Javascript",
-      });
-    }, 5000);
-    pannel.webview.html = getWebviewContent();
-    pannel.webview.onDidReceiveMessage(
-      ({ message }) => {
-        vscode.window.showInformationMessage(message);
-      },
-      undefined,
-      context.subscriptions
-    );
+    // setTimeout(() => {
+    //   pannel.webview.postMessage({
+    //     message: "Maxwells Loves Java and Javascript",
+    //   });
+    // }, 5000);
+
+    pannel.webview.html = getWebviewContent(context);
+    // pannel.webview.onDidReceiveMessage(
+    //   ({ message }) => {
+    //     vscode.window.showInformationMessage(message);
+    //   },
+    //   undefined,
+    //   context.subscriptions
+    // );
   });
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(ds);
 
-  function getWebviewContent() {
-    return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Snippet Generator</title>
+  // Load the webview.html file
+  function getWebviewContent(context: vscode.ExtensionContext) {
+    // Get the location of the extension
+    const extensionPath = context.extensionPath;
+    // console.log("Extension Path : ", extensionPath);
+    const webviewFilePath = path.join(extensionPath, "media", "webview.html");
+    // console.log("Webview.html Path : ", webviewFilePath);
 
-        <style>
-        * {
-          margin:0px;
-          padding:0px;
-          box-sizing:border-box;
-        }
-
-        html,body {
-          width:100%;
-          height:100%;
-        }
-
-        body {
-          display:flex;
-          flex-direction:column;
-          align-items:center;
-          margin-top:32px;
-        }
-
-          button {
-            padding:8px 16px;
-            font-size: var(--vscode-editor-font-size);
-            background :var(--vscode-button-background);
-            border:var(--vscode-checkbox-border);
-            color: var(--vscode-button-foreground);
-          }
-          button:hover{
-            background: var(--vscode-button-hoverBackground);
-          }
-
-          button:focus{
-            outline:none;
-          }
-
-          input[type="text"]{
-            background:var(--vscode-input-background);
-            border:1px solid var(--vscode-button-background);
-            color: var(--vscode-input-foreground);
-          }
-
-          input[type="text"]:focus{
-            outline:none;
-            border: 1px solid var(--vscode-button-hoverBackground);
-          }
-
-          ul {
-            margin-top:32px;
-            list-style:none;
-            display:flex;
-            flex-direction:column;
-            align-items:center;
-            width:100%;
-          }
-
-          ul li {
-            padding:1rem;
-            background:var(--vscode-dropdown-background);
-            margin-top:4px;
-            width:90%;
-          }
-          ul li span {
-            color : color: var(--vscode-button-foreground);;
-            font-size: var(--vscode-editor-font-size);
-            font-weight:var(--vscode-editor-font-weight);
-            font-family:var(--vscode-editor-font-family);
-          }
-
-        </style>
-    </head>
-    <body>
-          <button>Add Snippet</button>
-          <div style="display:flex; flex-direction:column; margin-top:32px;">
-            <input type="text" placeholder="Enter Text ... " style="padding:8px 16px; font-size:1.5rem"/>
-          </div>
-
-          <ul>
-            <li><span>List Item One</span></li>
-            <li><span>List Item Two</span></li>
-            <li><span>List Item Three</span></li>
-            <li><span>List Item Four</span></li>
-            <li><span>List Item Five</span></li>
-            <li><span>List Item One</span></li>
-            <li><span>List Item Two</span></li>
-            <li><span>List Item Three</span></li>
-            <li><span>List Item Four</span></li>
-            <li><span>List Item Five</span></li>
-            <li><span>List Item One</span></li>
-            <li><span>List Item Two</span></li>
-            <li><span>List Item Three</span></li>
-            <li><span>List Item Four</span></li>
-            <li><span>List Item Five</span></li>
-            <li><span>List Item One</span></li>
-            <li><span>List Item Two</span></li>
-            <li><span>List Item Three</span></li>
-            <li><span>List Item Four</span></li>
-            <li><span>List Item Five</span></li>
-            <li><span>List Item One</span></li>
-            <li><span>List Item Two</span></li>
-            <li><span>List Item Three</span></li>
-            <li><span>List Item Four</span></li>
-            <li><span>List Item Five</span></li>
-          </ul>
-    </body>
-    </html>`;
+    return fs.readFileSync(webviewFilePath, { encoding: "utf-8" });
   }
 }
 
